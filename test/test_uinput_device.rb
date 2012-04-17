7d12624fbefc76c8dcf6d4c00566ff393de4d6e3
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-17 22:55:46 +0900

    test for uinput_device.rb

---
+++ test/test_uinput_device.rb
@@ -0,0 +1,13 @@
+# -*- coding:utf-8; mode:ruby; -*-
+
+require 'test/unit'
+require 'rubygems'
+require 'revdev'
+require 'ruinput'
+
+class UinputDeviceTest < Test::Unit::TestCase
+  include Ruinput
+
+
+
+end
