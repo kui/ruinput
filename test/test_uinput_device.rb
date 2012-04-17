e039f6176eee5f16571293b4ac689740ba0d4a18
Keiichiro Ui <keiichiro.ui@gmail.com>
2012-04-18 01:51:16 +0900

    implemented uinput_device test

--- test/test_uinput_device.rb
+++ test/test_uinput_device.rb
@@ -8,6 +8,47 @@ require 'ruinput'
 class UinputDeviceTest < Test::Unit::TestCase
   include Ruinput
 
+  FILE = "/tmp/foo"
+  FileUtils.touch FILE
 
+  def test_init
+    UinputDevice.new FILE
+  end
+
+  def test_create
+    $file = File.new FILE
+    $ioctls = []
+    def $file.ioctl cmd, val=nil
+      $ioctls << cmd
+    end
+    $syswrites = ""
+    def $file.syswrite arg
+      $syswrites << arg
+    end
+
+    ud = UinputDevice.new $file
+    assert_nothing_raised do
+      ud.create
+    end
+    assert_equal UI_DEV_CREATE, $ioctls.last
+    assert 0 < $syswrites.length
+  end
+
+  def test_create
+    $file = File.new FILE
+    $ioctls = []
+    def $file.ioctl cmd, val=nil
+      $ioctls << cmd
+    end
+    def $file.syswrite arg
+    end
+
+    ud = UinputDevice.new $file
+    assert_nothing_raised do
+      ud.create
+      ud.destroy
+    end
+    assert_equal UI_DEV_DESTROY, $ioctls.last
+  end
 
 end
